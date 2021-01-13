4b84c2a05a5feec09200baaf4b5959acc442f472
Mehdi Hassan Jony <shoyshab@gmail.com>
2021-01-14 03:17:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# single-page-app
+ Using laravel and vue.js
---
+++ real-time-single-page-application-master/app/Http/Controllers/QuestionController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\QuestionResource;
+use App\Model\Question;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return QuestionResource::collection(Question::latest()->get());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+       return new QuestionResource($question);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // auth()->user()->question()->create($request->all());
+       Question::create($request->all());
+       return response('Created', Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
